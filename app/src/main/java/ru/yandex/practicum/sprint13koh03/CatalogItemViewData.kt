0ffc2e087c5e12d515cbe12abd9bf65d76d347db
JudjinGM <judjingm@gmail.com>
2023-04-18 21:33:31 +0400

    6 Добавить валюту (Р) в отображение цены

--- app/src/main/java/ru/yandex/practicum/sprint13koh03/CatalogItemViewData.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh03/CatalogItemViewData.kt
@@ -33,7 +33,7 @@ class CatalogItemViewHolder(
             .load(viewData.item.imageUrl)
             .into(binding.image)
         binding.title.text = viewData.item.name
-        binding.price.text = "${viewData.item.price / 100}/${viewData.item.unit}"
+        binding.price.text = "${viewData.item.price / 100} Р/${viewData.item.unit}"
 
         if (viewData.count != null) {
             binding.addToCart.visibility = View.GONE
